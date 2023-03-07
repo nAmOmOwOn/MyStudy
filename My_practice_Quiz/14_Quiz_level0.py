cb2ff354c94e5164537327fbbdac8ea76901580b
jw <malinup@naver.com>
2023-03-07 09:31:07 +0900

    commit

---
+++ My_practice_Quiz/14_Quiz_level0.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120924
+
+def solution(common):
+    if common[1] % common[0] and common[2] % common[1] == 0:
+        return int(common[1]/common[0]*common[len(common)-1])
+    else:
+        return int(common[len(common)-1] + (common[1] - common[0]))
+
+common = [1, 2, 3, 4]
+
+print(solution(common))
