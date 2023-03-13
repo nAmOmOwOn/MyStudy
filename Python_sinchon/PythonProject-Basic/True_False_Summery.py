30accc45d89cb5a555dedff2936c08750a16f0a8
jw <malinup@naver.com>
2023-03-13 15:21:13 +0900

    commit

---
+++ Python_sinchon/PythonProject-Basic/True_False_Summery.py
@@ -0,0 +1,9 @@
+True and True == True
+False and True == False
+True and False == False
+False and False == False
+
+True or True == True
+True or False == True
+False or True == True
+False or False == False
