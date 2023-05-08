a7fe7f0231863339b2bae3aec48e0de50b5b8a47
jw <malinup@naver.com>
2023-05-08 09:08:26 +0900

    commit

--- My_practice_Quiz/05_Quiz_level0.py
+++ My_practice_Quiz/05_Quiz_level0.py
@@ -1,18 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120884
 # 치킨 쿠폰
 
-
 def solution(chicken):
-    bonus = chicken // 10
-    chicken += bonus
-    while True:
-        if bonus >= 10:
-            chicken += bonus // 10
-            bonus = bonus // 10
-        else:
-            break
-    return chicken // 10
+    answer = 0
+    coupon = chicken
 
+    while coupon >= 10:
+        eaten = coupon//10 
+        answer += eaten 
+        coupon = coupon%10 + eaten 
+    return answer 
 
 chicken = 1081
 
